0aeda0a91e8e8c2a9b600a3c28c914ecf402e729
Tyran Christian <tyranchristian010@gmail.com>
2020-05-29 18:02:47 -0700

    third commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,15 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "upper manage traffic trigger juice truth client universe involve across addict gap";
+
 require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/dedbade6d9fc42ab98cf80dcd71a3c42")
+      },
       network_id: "*" // Match any network id
     },
   },
